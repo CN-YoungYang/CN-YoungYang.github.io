ddbac66f36b2bc36133a00166afbbfd2ff16e0a0
CN-YoungYang <25340441+CN-YoungYang@users.noreply.github.com>
2024-10-15 08:45:36 +0800

    Update webSideBar.ts

--- .vuepress/sidebars/webSideBar.ts
+++ .vuepress/sidebars/webSideBar.ts
@@ -4,7 +4,7 @@ export default [
     title: "HTML5",
     collapsable: true,
     children: [
-      "./HTML5/HTML5_Kimi自动删除历史记录md",
+      "./HTML5/HTML5_Kimi自动删除历史记录.md",
       "./HTML5/HTML5_响应式列表宽度.md",
     ],
   },

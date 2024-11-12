e2f1ad32a0efea08125cef7e05f74bf1b5ebf29c
YoungYang <25340441+CN-YoungYang@users.noreply.github.com>
2024-11-12 21:28:42 +0800

    Flutter 基础特性 路由设置

--- .vuepress/sidebars/flutterSideBar.ts
+++ .vuepress/sidebars/flutterSideBar.ts
@@ -1,10 +1,33 @@
 export default [
   "",
   {
-    title: "Flutter",
-    collapsable: false,
+    title: "基础特性",
+    collapsable: true,
     children: [
-      "测试文章2.md",
+      "./基础特性/Flutter_目录结构.md",
+      "./基础特性/Flutter_组件.md",
+      "./基础特性/Flutter_调试工具.md",
+      "./基础特性/Flutter_布局约束规则.md",
+      "./基础特性/Flutter_松约束.md",
+      "./基础特性/Flutter_紧约束.md",
+      "./基础特性/Flutter_无边界.md",
+      "./基础特性/Flutter_有状态无状态组件.md",
+      "./基础特性/Flutter_生命周期.md",
     ],
   },
+  {
+    title: "容器布局",
+    collapsable: true,
+    children: [],
+  },
+  {
+    title: "常用组件",
+    collapsable: true,
+    children: [],
+  },
+  {
+    title: "布局练习",
+    collapsable: true,
+    children: [],
+  }
 ];
---
+++ docs/flutter/基础特性/Flutter_调试工具.md
@@ -0,0 +1,6 @@
+# devTools 调试工具
+
+> 本文作者：[阳九五](https://github.com/CN-YoungYang)
+>
+> 本站地址：[https://blog.59young.com](https://blog.59young.com)
+

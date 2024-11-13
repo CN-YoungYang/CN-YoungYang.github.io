6d98ad803bbfeea94538e760c8ac1589702f5340
YoungYang <25340441+CN-YoungYang@users.noreply.github.com>
2024-11-14 00:15:23 +0800

    还原  设置

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@alias': 'https://raw.githubusercontent.com/CN-YoungYang/BlogAssets/refs/heads/master'
-      }
-    }
-  }
-}
